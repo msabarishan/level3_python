597dc04cebf2e061c93379c8e6b2d45df89392dc
msabarishan <103972490+msabarishan@users.noreply.github.com>
2023-06-01 09:18:21 +0530

    Create string_operation.py

---
+++ string_operation.py
@@ -0,0 +1,63 @@
+# COnverting upper string to lower and vice versa
+
+def swap_case(s):
+    f=''
+    for char in s:
+        if char.isupper():
+            f+=char.lower()
+        else:
+            f+=char.upper()
+    return f
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+    
+# Mutate String
+def mutate_string(string, position, character):
+    string = string[:position] + character + string[position+1:]
+    return
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+    
+# Testing the string
+str.isalnum()
+This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
+
+>>> print 'ab123'.isalnum()
+True
+>>> print 'ab123#'.isalnum()
+False
+str.isalpha()
+This method checks if all the characters of a string are alphabetical (a-z and A-Z).
+
+>>> print 'abcD'.isalpha()
+True
+>>> print 'abcd1'.isalpha()
+False
+str.isdigit()
+This method checks if all the characters of a string are digits (0-9).
+
+>>> print '1234'.isdigit()
+True
+>>> print '123edsd'.isdigit()
+False
+str.islower()
+This method checks if all the characters of a string are lowercase characters (a-z).
+
+>>> print 'abcd123#'.islower()
+True
+>>> print 'Abcd123#'.islower()
+False
+str.isupper()
+This method checks if all the characters of a string are uppercase characters (A-Z).
+
+>>> print 'ABCD123#'.isupper()
+True
+>>> print 'Abcd123#'.isupper()
+False
